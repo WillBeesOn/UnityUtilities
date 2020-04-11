7018c7280104ef3e08052864bc2e85b3adba7103
Willem Beeson <willemb@MacBook-Pro.local>
2020-04-11 14:18:42 -0400

    moved entitites to its repo

---
+++ Physics/Raycast.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityUtilities {
+
+    public static class Physics {
+
+        public static GameObject Raycast(Ray r) {
+            RaycastHit hit = new RaycastHit();
+            if (UnityEngine.Physics.Raycast(r, out hit)) {
+                return hit.transform.gameObject;
+            }
+            return default;
+        }
+    }
+}
