6beef90f3958c725d5fc95718d7d1499eb6719ca
Willem Beeson <willemb@MacBook-Pro.local>
2020-04-17 12:46:19 -0400

    ove some usings

--- Basic/Utils.cs
+++ Basic/Utils.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using UnityEngine;
-using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
 namespace UnityUtilities {
